b55e989f4f30f3e8f4f105636660aa89d492c7c3
taha27 <tahashabbir27@gmail.com>
2017-07-26 01:10:08 -0400

    updated to define non-method callback function

--- learnyounode/make_it_modular.js
+++ learnyounode/make_it_modular.js
@@ -1,13 +1,11 @@
 const filtered_ls = require('./filtered_ls_module');
 
-function printData(err, data) {
+filtered_ls(process.argv[2], process.argv[3], (err, list) => {
     if (err) {
         return console.error(err);
     };
-    for (let file of data) {
+    for (let file of list) {
         console.log(file);
-    }
-};
-
-filtered_ls(process.argv[2], process.argv[3], printData)
+    };
+});
 
